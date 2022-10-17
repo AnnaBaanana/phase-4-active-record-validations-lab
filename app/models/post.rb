516c40e1b009e15d9e6b4cced00accff7267f78a
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-10-17 10:00:00 -0400

    commit tree 9de1881596730e45f7528d75e49138cbe193b91a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: { in: 0..250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    #private validation method
+    validate :title_valid
+
+    def title_valid
+        if title == 'True Facts'
+            errors.add(:title, "cannot be True Facts")
+        end
+    end
+
 end
